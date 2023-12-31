688efb31c7b28958497e098780acd1f9ac5db747
Adam Peretti <adam.peretti@gmail.com>
2023-12-31 09:25:38 -0800

    Update avatars

--- frontend/app/components/ui/chat/chat-avatar.tsx
+++ frontend/app/components/ui/chat/chat-avatar.tsx
@@ -20,13 +20,13 @@ export default function ChatAvatar(message: Message) {
   }
 
   return (
-    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border  bg-black text-white">
+    <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-white text-white">
       <Image
         className="rounded-md"
-        src="/llama.png"
-        alt="Llama Logo"
-        width={24}
-        height={24}
+        src="/ai.png"
+        alt="IRP Bot"
+        width={36}
+        height={36}
         priority
       />
     </div>
